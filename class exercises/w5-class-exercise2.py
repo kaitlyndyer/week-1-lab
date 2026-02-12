3296fc495777e8dd1e74869bfec542dd656be2da
Kaitlyn Dyer <dyer.kai@northeastern.edu>
2026-02-12 15:12:10 +0000

    W5 Lecture 2 continued

--- class exercises/w5-class-exercise2.py
+++ class exercises/w5-class-exercise2.py
@@ -1,18 +1,49 @@
 # CLASS EXERCISES
 ## playlist
 class Playlist:
-    __slots__ = []
+    __slots__ = ['_name', '_songs']
+    total_songs_added = 0
 
     def __init__(self, name):
         self.name = name
+        self._songs = []
 
     @property
     def name(self):
         return self._name
     
     @name.setter
-    def name(self, song):
-        if song == "":
-            raise ValueError(f'Song must be non-empty string, got {song}')
+    def name(self, value):
+        if not isinstance(value, str) or value == "":
+            raise ValueError(f'Name must be non-empty string, got {value}')
+        self._name = value
+
+    @property
+    def songs(self):
+        return self._songs.copy()
+    
+    def add_song(self, song: str):
+        if song in self._songs:
+            raise ValueError(f'The song "{song}" is already in the playlist')
+        self._songs.append(song)
+        Playlist.total_songs_added += 1
+    
+    @classmethod
+    def get_total_songs_added(cls):
+        return cls.total_songs_added
+
+pl = Playlist("Chill")
+pl.add_song("K.K. Cruisin")
+pl.add_song("K.K. Folk")
+
+print(pl.songs)
+
+pl.songs.append("Dont add!")
+print(len(pl.songs))
+
+print(Playlist.get_total_songs_added())
+
+    
+        
     
 
